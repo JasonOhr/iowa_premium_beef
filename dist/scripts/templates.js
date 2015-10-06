288bdedbdfeb953a9895672e38c31cf7cd862eda
Jason Ohr <jason.ohr@gmail.com>
2015-10-06 10:24:20 -0400

    Set up basic header positioning

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -1,7 +1,10 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Hello</h1>\n";
+    return "<div class=\"top-image\">\n    <img src=\"dist/images/dinner.jpeg\" alt=\"dinner\"/>\n</div>\n<h1>Hello</h1>\n";
 },"useData":true});
 this["JST"]["header"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<header>\n  <div class=\"container\">\n      <img src=\"dist/images/logo.png\" alt=\"logo\"/>\n\n  </div>\n</header>";
+    return "<div class=\"header-wrapper\">\n    <header>\n        <div class=\"container\">\n            <div class=\"header-logo\">\n                <img src=\"dist/images/logo.png\" alt=\"logo\"/>\n            </div>\n        </div>\n    </header>\n    <div class=\"shadow\"></div>\n</div>\n";
+},"useData":true});
+this["JST"]["top-navigation"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "";
 },"useData":true});
