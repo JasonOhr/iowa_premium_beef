589e5696448a70ba566a702b61d40de127d5b878
Jason Ohr <jason.ohr@gmail.com>
2015-10-06 09:27:27 -0400

    Added some image files and basic css

---
+++ dist/scripts/templates.js
@@ -0,0 +1,7 @@
+this["JST"] = this["JST"] || {};
+this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Hello</h1>\n";
+},"useData":true});
+this["JST"]["header"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<header>\n  <div class=\"container\">\n      <img src=\"dist/images/logo.png\" alt=\"logo\"/>\n\n  </div>\n</header>";
+},"useData":true});
