5a504c24148c224c7024a830db1bf56158545add
Jason Ohr <jason.ohr@gmail.com>
2015-10-06 16:04:22 -0400

    I have the nav bar working

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -2,9 +2,12 @@ this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<div class=\"top-image\">\n    <img src=\"dist/images/dinner.jpeg\" alt=\"dinner\"/>\n</div>\n<h1>Hello</h1>\n";
 },"useData":true});
+this["JST"]["footer"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "";
+},"useData":true});
 this["JST"]["header"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<div class=\"header-wrapper\">\n    <header>\n        <div class=\"container\">\n            <div class=\"header-logo\">\n                <img src=\"dist/images/logo.png\" alt=\"logo\"/>\n            </div>\n        </div>\n    </header>\n    <div class=\"shadow\"></div>\n</div>\n";
+    return "<div class=\"header-wrapper\">\n    <header>\n        <div class=\"container\">\n            <div class=\"header-logo\">\n                <img src=\"dist/images/logo.png\" alt=\"logo\"/>\n            </div>\n            \n        </div>\n    </header>\n    <div class=\"shadow\"></div>\n</div>\n";
 },"useData":true});
 this["JST"]["top-navigation"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
+    return "<nav class=\"top-nav\">\n    <ul>\n        <li><a href=\"#\">about us</a></li>\n        <li class=\"top-drop-down\"><a href=\"#\">our story</a>\n            <ul class=\"drop-down\">\n                <li><a href=\"#\">here</a></li>\n                <li><a href=\"#\">there</a></li>\n            </ul>\n        </li>\n        <li><a href=\"#\">our product</a></li>\n        <li class=\"top-drop-down\"><a href=\"#\">join our team</a>\n            <ul class=\"drop-down\">\n                <li><a href=\"#\">Holy shit Dawg</a></li>\n                <li><a href=\"#\">Holy Guacamole Batman</a></li>\n                <li><a href=\"#\">What the F*/!? Dude</a></li>\n            </ul>\n        </li>\n        <li><a href=\"#\">contact us</a></li>\n    </ul>\n</nav>";
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,19 @@
 
   $(document).ready(function(){
     $('body').prepend(JST['application']()).prepend(JST['header']());
+    $('.header-wrapper .container').append(JST['top-navigation']());
+
+    $('li.top-drop-down ').mouseenter(
+        function(){
+          $(this).find('ul.drop-down').slideDown();
+        }
+    );
+    $('li.top-drop-down').mouseleave(
+        function(){
+          $(this).find('ul.drop-down').slideUp();
+        }
+    );
 
   });
 })();
+
